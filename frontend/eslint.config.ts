1bcaa46611060d1e493c8debab1d887da4656bb2
Tauquir <30658453+itstauq@users.noreply.github.com>
2026-03-10 01:42:00 +0530

    chore: add Cypress eslint plugin to antfu config

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -1,11 +1,21 @@
 import antfu from '@antfu/eslint-config'
+import pluginCypress from 'eslint-plugin-cypress'
 
-export default antfu({
-  type: 'app',
-  vue: true,
-  typescript: true,
-  stylistic: {
-    indent: 2,
-    quotes: 'single',
+export default antfu(
+  {
+    type: 'app',
+    vue: true,
+    typescript: true,
+    stylistic: {
+      indent: 2,
+      quotes: 'single',
+    },
   },
-})
+  {
+    ...pluginCypress.configs.recommended,
+    files: [
+      'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
+      'cypress/support/**/*.{js,ts,jsx,tsx}',
+    ],
+  },
+)
